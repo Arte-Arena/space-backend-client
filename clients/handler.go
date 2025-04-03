55dacc84a53bbf5fd64d846c5ebe55e91c254e0f
Pablo Yuri <pabloyuridev@gmail.com>
2025-04-03 15:25:38 -0300

    Add update handler to clients route

--- clients/handler.go
+++ clients/handler.go
@@ -175,7 +175,200 @@ func create(w http.ResponseWriter, r *http.Request) {
 }
 
 func update(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(middlewares.UserIDKey)
+	if userId == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: "Usuário não autorizado",
+		})
+		return
+	}
 
+	clientFromRequest := schemas.ClientUpdateRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&clientFromRequest); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.CLIENTS_INVALID_REQUEST_DATA),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), database.MONGODB_TIMEOUT)
+	defer cancel()
+
+	mongoURI := os.Getenv(utils.ENV_MONGODB_URI)
+	opts := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(opts)
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.CANNOT_CONNECT_TO_MONGODB),
+		})
+		return
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	collection := mongoClient.Database(database.MONGODB_DB_ADMIN).Collection("clients")
+
+	userIdStr, ok := userId.(string)
+	if !ok {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.INVALID_USER_ID_FORMAT),
+		})
+		return
+	}
+
+	objectId, err := utils.ParseObjectIDFromHex(userIdStr)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.INVALID_USER_ID_FORMAT),
+		})
+		return
+	}
+
+	filter := bson.D{{Key: "_id", Value: objectId}}
+	client := schemas.ClientFromDB{}
+	err = collection.FindOne(ctx, filter).Decode(&client)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(schemas.ApiResponse{
+				Message: "Cliente não encontrado",
+			})
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.ERROR_TO_TRY_FIND_MONGODB),
+		})
+		return
+	}
+
+	if clientFromRequest.Email != "" && clientFromRequest.Email != client.Contact.Email {
+		emailFilter := bson.D{{Key: "contact.email", Value: clientFromRequest.Email}}
+		existingClient := schemas.ClientFromDB{}
+		err = collection.FindOne(ctx, emailFilter).Decode(&existingClient)
+		if err == nil {
+			w.WriteHeader(http.StatusConflict)
+			json.NewEncoder(w).Encode(schemas.ApiResponse{
+				Message: "Email já cadastrado",
+			})
+			return
+		}
+	}
+
+	updateFields := bson.D{}
+
+	if clientFromRequest.Password != "" {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(clientFromRequest.Password), bcrypt.DefaultCost)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(schemas.ApiResponse{
+				Message: utils.SendInternalError(utils.ERROR_TO_CREATE_PASSWORD_HASH),
+			})
+			return
+		}
+		updateFields = append(updateFields, bson.E{Key: "password_hash", Value: string(hashedPassword)})
+	}
+
+	if clientFromRequest.Name != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.name", Value: clientFromRequest.Name})
+	}
+	if clientFromRequest.Email != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.email", Value: clientFromRequest.Email})
+	}
+	if clientFromRequest.PersonType != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.person_type", Value: clientFromRequest.PersonType})
+	}
+	if clientFromRequest.IdentityCard != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.identity_card", Value: clientFromRequest.IdentityCard})
+	}
+	if clientFromRequest.CPF != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.cpf", Value: clientFromRequest.CPF})
+	}
+	if clientFromRequest.CellPhone != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.cell_phone", Value: clientFromRequest.CellPhone})
+	}
+	if clientFromRequest.ZipCode != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.zip_code", Value: clientFromRequest.ZipCode})
+	}
+	if clientFromRequest.Address != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.address", Value: clientFromRequest.Address})
+	}
+	if clientFromRequest.Number != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.number", Value: clientFromRequest.Number})
+	}
+	if clientFromRequest.Complement != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.complement", Value: clientFromRequest.Complement})
+	}
+	if clientFromRequest.Neighborhood != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.neighborhood", Value: clientFromRequest.Neighborhood})
+	}
+	if clientFromRequest.City != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.city", Value: clientFromRequest.City})
+	}
+	if clientFromRequest.State != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.state", Value: clientFromRequest.State})
+	}
+	if clientFromRequest.CompanyName != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.company_name", Value: clientFromRequest.CompanyName})
+	}
+	if clientFromRequest.CNPJ != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.cnpj", Value: clientFromRequest.CNPJ})
+	}
+	if clientFromRequest.StateRegistration != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.state_registration", Value: clientFromRequest.StateRegistration})
+	}
+	if clientFromRequest.BillingZipCode != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_zip_code", Value: clientFromRequest.BillingZipCode})
+	}
+	if clientFromRequest.BillingAddress != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_address", Value: clientFromRequest.BillingAddress})
+	}
+	if clientFromRequest.BillingNumber != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_number", Value: clientFromRequest.BillingNumber})
+	}
+	if clientFromRequest.BillingComplement != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_complement", Value: clientFromRequest.BillingComplement})
+	}
+	if clientFromRequest.BillingNeighborhood != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_neighborhood", Value: clientFromRequest.BillingNeighborhood})
+	}
+	if clientFromRequest.BillingCity != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_city", Value: clientFromRequest.BillingCity})
+	}
+	if clientFromRequest.BillingState != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.billing_state", Value: clientFromRequest.BillingState})
+	}
+	updateFields = append(updateFields, bson.E{Key: "contact.different_billing_address", Value: clientFromRequest.DifferentBillingAddress})
+	if clientFromRequest.Status != "" {
+		updateFields = append(updateFields, bson.E{Key: "contact.status", Value: clientFromRequest.Status})
+	}
+
+	updateFields = append(updateFields, bson.E{Key: "contact.updated_at", Value: time.Now()})
+
+	if len(updateFields) <= 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: "Nenhum campo para atualizar",
+		})
+		return
+	}
+
+	update := bson.D{{Key: "$set", Value: updateFields}}
+
+	_, err = collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(schemas.ApiResponse{
+			Message: utils.SendInternalError(utils.ERROR_TO_UPDATE_CLIENT_TO_MONGODB),
+		})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
--- schemas/clients.go
+++ schemas/clients.go
@@ -33,6 +33,35 @@ type ClientCreateRequest struct {
 	Password string `json:"password" bson:"-"`
 }
 
+type ClientUpdateRequest struct {
+	Name                    string `json:"name,omitempty"`
+	Email                   string `json:"email,omitempty"`
+	Password                string `json:"password,omitempty"`
+	PersonType              string `json:"person_type,omitempty"`
+	IdentityCard            string `json:"identity_card,omitempty"`
+	CPF                     string `json:"cpf,omitempty"`
+	CellPhone               string `json:"cell_phone,omitempty"`
+	ZipCode                 string `json:"zip_code,omitempty"`
+	Address                 string `json:"address,omitempty"`
+	Number                  string `json:"number,omitempty"`
+	Complement              string `json:"complement,omitempty"`
+	Neighborhood            string `json:"neighborhood,omitempty"`
+	City                    string `json:"city,omitempty"`
+	State                   string `json:"state,omitempty"`
+	CompanyName             string `json:"company_name,omitempty"`
+	CNPJ                    string `json:"cnpj,omitempty"`
+	StateRegistration       string `json:"state_registration,omitempty"`
+	BillingZipCode          string `json:"billing_zip_code,omitempty"`
+	BillingAddress          string `json:"billing_address,omitempty"`
+	BillingNumber           string `json:"billing_number,omitempty"`
+	BillingComplement       string `json:"billing_complement,omitempty"`
+	BillingNeighborhood     string `json:"billing_neighborhood,omitempty"`
+	BillingCity             string `json:"billing_city,omitempty"`
+	BillingState            string `json:"billing_state,omitempty"`
+	DifferentBillingAddress bool   `json:"different_billing_address,omitempty"`
+	Status                  string `json:"status,omitempty"`
+}
+
 type ClientCreateModel struct {
 	Contact      Contact   `bson:"contact"`
 	PasswordHash string    `bson:"password_hash"`
