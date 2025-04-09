5fb0ba0f584d6fbc1198c0bb3067bfcaca84062e
Pablo Yuri <pabloyuridev@gmail.com>
2025-04-09 14:16:38 -0300

    Improve CORS configuration

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,15 +1,23 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+	"slices"
+)
 
 func Cors(next http.Handler) http.Handler {
+	allowedOrigins := []string{
+		"http://localhost:8000",
+		"http://localhost:3000",
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		if origin == "" {
-			origin = "http://localhost:3000"
+
+		if slices.Contains(allowedOrigins, origin) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 
-		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
