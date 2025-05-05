402fbf8d335d0eb80fc01528d885b3365f872cd1
Leandro Damasio <ldamasio@gmail.com>
2025-05-05 17:38:37 -0300

    debug: Add logs to webhook handler (6)

--- space-backend-client/middlewares/logging.go
+++ space-backend-client/middlewares/logging.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -10,8 +12,19 @@ func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		// Lê e loga payload da requisição sem consumir r.Body permanentemente
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("[Logging] erro ao ler body: %v", err)
+		} else {
+			log.Printf("[Logging] payload recebido: %s", string(bodyBytes))
+		}
+		// Reposiciona r.Body para próxima leitura pelo handler
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
 		next.ServeHTTP(w, r)
 
+		// Log final com método, URI, remoto e duração
 		log.Printf(
 			"%s %s %s %s",
 			r.Method,
